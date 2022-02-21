4176b8c69195e9c557d051907ffc38702be4121a
James Henry <james.henry.purnama0@gmail.com>
2022-02-21 20:44:14 +0700

    Working Mobile TODO: 3D Hero Clean Desktop Layout About Page Fix scrolltrigger Reel Modal

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,12 @@
       <h1>james</h1>
       <h1>henry</h1>
     </span>
-    <!-- <p v-for="obj in getObjects.objects" :key="obj.title">
-      {{ obj }}
-    </p> -->
+    <button v-if="windowWidth <= 600" class="about"><p>about</p></button>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   beforeRouteLeave(to, _from, next) {
@@ -20,7 +18,10 @@ export default defineComponent({
     next()
   },
   setup() {
-    return {}
+    const windowWidth = computed(() => window.innerWidth)
+    return {
+      windowWidth,
+    }
   },
 })
 </script>
@@ -28,17 +29,58 @@ export default defineComponent({
 <style lang="scss" scoped>
 section {
   display: flex;
-  padding: 50px;
+  z-index: 1;
+  position: relative;
+  padding: 20px;
+  height: 100vh;
+  width: 100vw;
+  min-height: -webkit-fill-available;
+
+  @include min-media(mobile) {
+    padding: 50px;
+  }
 
   span {
     h1 {
-      font-size: 100px;
+      font-size: 4em;
       line-height: 0.8em;
+
+      @include min-media(mobile) {
+        font-size: 100px;
+        line-height: 0.8em;
+      }
 
       &:last-child {
         position: relative;
-        left: 0.3em;
+        left: 0.2em;
+
+        @include min-media(mobile) {
+          left: 0.3em;
+        }
       }
+    }
+  }
+
+  .about {
+    color: white;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    margin: 20px;
+    border: 1px solid white;
+    border-radius: 20px 20px;
+    background-color: rgba(0, 0, 0, 0);
+    transition: background color 0.5s ease;
+    cursor: pointer;
+
+    p {
+      font-size: 1.2em;
+      padding: 0.5em;
+    }
+
+    &:hover {
+      color: black;
+      background-color: white;
     }
   }
 }
