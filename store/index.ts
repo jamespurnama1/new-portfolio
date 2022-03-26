4f3926d05c50e10a65c8ec3403a111c280dccb54
jamespurnama1 <jamespurnama1@users.noreply.github.com>
2022-03-26 21:11:34 +0700

    add check remove works loading

--- store/index.ts
+++ store/index.ts
@@ -2,12 +2,11 @@
 import { defineStore } from 'pinia'
 
 export const useStore = defineStore('storeId', {
-  // arrow function recommended for full type inference
   state: () => ({
     windowWidth: 0 as number,
     opened: false as boolean,
     loadHome: 0 as number,
-    loadWorks: 0 as number,
+    loadWorks: 100 as number, // disabled
     loadWebGL: 0 as number,
     cache: [{}] as Array<{
       title: string
