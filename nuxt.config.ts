f96a1d118de2925cee97ff341936733146eb8f57
jamespurnama1 <jamespurnama1@users.noreply.github.com>
2022-03-26 22:37:20 +0700

    fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     // script: {
     //  src: './js/GSDevTools.min.js',
     // },
